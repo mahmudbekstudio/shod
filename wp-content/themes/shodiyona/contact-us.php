e58295be04f3490acbd036264a48fa052b6308c0
Mahmudbek Odilov <modilov@dealerfire.com>
2016-05-06 12:18:06 +0500

    contact page

---
+++ wp-content/themes/shodiyona/contact-us.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Template Name: Contact us
+*/
+get_header(); ?>
+<div id="contact-page" class="container">
+	<div class="bg">
+		<div class="row">
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<div class="col-sm-8">
+				<div class="contact-form">
+					<div id="main-contact-form" class="contact-form row">
+						<h2 class="title text-center">Get In Touch</h2>
+						<?php echo do_shortcode(get_field('contact_form_code')); ?>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="contact-info">
+					<h2 class="title text-center">Contact Info</h2>
+					<address>
+						<?php the_content(); ?>
+					</address>
+				</div>
+			</div>
+			<?php  endwhile;endif; ?>
+		</div>
+	</div>
+</div><!--/#contact-page-->
+<?php get_footer(); ?>
+
