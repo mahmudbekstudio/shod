18e575b3a08385dd9dfa15a27637c49519902e3f
amirsaidhon <amirsaidhon@gmail.com>
2016-05-07 18:13:42 +0500

    shaharlar

--- wp-content/themes/shodiyona/home.php
+++ wp-content/themes/shodiyona/home.php
@@ -7,10 +7,10 @@ get_header(); ?>
 	<div class="swiper-wrapper">
 		<div class="swiper-slide item">
 			<div class="col-sm-6">
-				<h1><span>E</span>-SHOPPER</h1>
-				<h2>Free E-Commerce Template</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-				<button type="button" class="btn btn-default get">Get it now</button>
+<!--				<h1><span>E</span>-SHOPPER</h1>-->
+				<h2>Аренда автомобилей на свадьбу или другое торжество</h2>
+				<p>Вы можете рекламировать свои услуги и товары.</p>
+<!--				<button type="button" class="btn btn-default get">Get it now</button>-->
 			</div>
 			<div class="col-sm-6">
 				<img src="<?php bloginfo( 'template_directory' ); ?>/images/home/mersedes.jpg" class="girl img-responsive" alt="" />
@@ -19,10 +19,10 @@ get_header(); ?>
 		</div>
 		<div class="swiper-slide item">
 			<div class="col-sm-6">
-				<h1><span>E</span>-SHOPPER</h1>
-				<h2>100% Responsive Design</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-				<button type="button" class="btn btn-default get">Get it now</button>
+<!--				<h1><span>E</span>-SHOPPER</h1>-->
+				<h2>Цветы</h2>
+				<p>Цветы смотрятся очень нежно и вносят в образ невесты нотку романтики. </p>
+<!--				<button type="button" class="btn btn-default get">Get it now</button>-->
 			</div>
 			<div class="col-sm-6">
 				<img src="<?php bloginfo( 'template_directory' ); ?>/images/home/flowers.png" class="girl img-responsive" alt="" />
@@ -31,50 +31,50 @@ get_header(); ?>
 		</div>
 		<div class="swiper-slide item">
 			<div class="col-sm-6">
-				<h1><span>E</span>-SHOPPER</h1>
-				<h2>Free Ecommerce Template</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-				<button type="button" class="btn btn-default get">Get it now</button>
+<!--				<h1><span>E</span>-SHOPPER</h1>-->
+				<h2>Свадебные салоны</h2>
+				<p>Лучший свадебный салоны, откуда уходят только довольные и улыбающиеся невесты с платьем мечты! </p>
+<!--				<button type="button" class="btn btn-default get">Get it now</button>-->
 			</div>
 			<div class="col-sm-6">
-				<img src="<?php bloginfo( 'template_directory' ); ?>/images/home/restaurant.png" class="girl img-responsive" alt="" />
+				<img src="<?php bloginfo( 'template_directory' ); ?>/images/home/weddingdress.png" class="girl img-responsive" alt="" />
 <!--				<img src="--><?php //bloginfo( 'template_directory' ); ?><!--/images/home/pricing.png" class="pricing" alt="" />-->
 			</div>
 		</div>
 		<div class="swiper-slide item">
 			<div class="col-sm-6">
-				<h1><span>E</span>-SHOPPER</h1>
-				<h2>Free E-Commerce Template</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-				<button type="button" class="btn btn-default get">Get it now</button>
+<!--				<h1><span>E</span>-SHOPPER</h1>-->
+				<h2>Тамада и Артисты</h2>
+				<p>Выбирайте любого артиста или тамаду, договаривайтесь, на сайте указаны телефоны их администраторов.</p>
+<!--				<button type="button" class="btn btn-default get">Get it now</button>-->
 			</div>
 			<div class="col-sm-6">
-				<img src="<?php bloginfo( 'template_directory' ); ?>/images/home/girl1.jpg" class="girl img-responsive" alt="" />
-				<img src="<?php bloginfo( 'template_directory' ); ?>/images/home/pricing.png"  class="pricing" alt="" />
-			</div>
-		</div>
-		<div class="swiper-slide item">
-			<div class="col-sm-6">
-				<h1><span>E</span>-SHOPPER</h1>
-				<h2>100% Responsive Design</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-				<button type="button" class="btn btn-default get">Get it now</button>
-			</div>
-			<div class="col-sm-6">
-				<img src="<?php bloginfo( 'template_directory' ); ?>/images/home/girl2.jpg" class="girl img-responsive" alt="" />
-				<img src="<?php bloginfo( 'template_directory' ); ?>/images/home/pricing.png"  class="pricing" alt="" />
+				<img src="<?php bloginfo( 'template_directory' ); ?>/images/home/microphone.png" class="girl img-responsive" alt="" />
+<!--				<img src="--><?php //bloginfo( 'template_directory' ); ?><!--/images/home/pricing.png"  class="pricing" alt="" />-->
 			</div>
 		</div>
 		<div class="swiper-slide item">
 			<div class="col-sm-6">
-				<h1><span>E</span>-SHOPPER</h1>
-				<h2>Free Ecommerce Template</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-				<button type="button" class="btn btn-default get">Get it now</button>
+<!--				<h1><span>E</span>-SHOPPER</h1>-->
+				<h2>Ювелирные Изделия</h2>
+				<p>Ювелирные украшения - это отличный подарок любой женщине в любом возрасте. Покупая ювелирные украшения для себя, или в подарок, надо понимать, что это вещь не одного дня и возможно будет переходить из поколения в поколение.</p>
+<!--				<button type="button" class="btn btn-default get">Get it now</button>-->
 			</div>
 			<div class="col-sm-6">
-				<img src="<?php bloginfo( 'template_directory' ); ?>/images/home/girl3.jpg" class="girl img-responsive" alt="" />
-				<img src="<?php bloginfo( 'template_directory' ); ?>/images/home/pricing.png" class="pricing" alt="" />
+				<img src="<?php bloginfo( 'template_directory' ); ?>/images/home/jewelery.png" class="girl img-responsive" alt="" />
+<!--				<img src="--><?php //bloginfo( 'template_directory' ); ?><!--/images/home/pricing.png"  class="pricing" alt="" />-->
+			</div>
+		</div>
+		<div class="swiper-slide item">
+			<div class="col-sm-6">
+<!--				<h1><span>E</span>-SHOPPER</h1>-->
+				<h2>Оформление ТОРЖЕСТВ</h2>
+				<p>Свадьба! Это один из самых долгожданных дней в жизни каждой влюбленной пары. Именно поэтому каждый хочет сделать украшение зала на свадьбу незабываемым!</p>
+<!--				<button type="button" class="btn btn-default get">Get it now</button>-->
+			</div>
+			<div class="col-sm-6">
+				<img src="<?php bloginfo( 'template_directory' ); ?>/images/home/oformleniya.png" class="girl img-responsive" alt="" />
+<!--				<img src="--><?php //bloginfo( 'template_directory' ); ?><!--/images/home/pricing.png" class="pricing" alt="" />-->
 			</div>
 		</div>
 	</div>
